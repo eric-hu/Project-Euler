4e851e88452e5988847b3cff87bdd36d8dcee076
Eric Hu <eric@lemurheavy.com>
2013-03-25 01:55:44 -0700

    Naive solution to problem 10
    Runtime takes 5-10 minutes.  This solution has lots of room for
    optimization.

---
+++ prob10.rb
@@ -0,0 +1,36 @@
+# Summation of primes
+# Problem 10
+#
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+
+# Result
+#
+# 142913828922
+# Current solution takes 5-10 minutes to run.  This problem has lots of room
+# for optimization.
+
+require './primes.rb'
+
+# =============================  Support Methods ==============================
+def array_of_primes_less_than limit
+
+  result = []
+
+  return result unless limit >= 2
+
+  (2...limit).each do |value|
+    puts "value is: #{value}" if value % 10000 == 0
+    result.push value if is_prime?(value)
+  end
+
+  result
+end
+
+# =================================  Solution ==================================
+
+upper_limit = 2_000_000
+result = array_of_primes_less_than(upper_limit).inject &:+
+
+puts "The sum of primes less than #{upper_limit} is #{result}"
