820de49707a591c4b1ea673ca57c9499cbc3bce0
Eric Hu <eric@lemurheavy.com>
2013-04-01 02:29:08 -0700

    Update primes.rb library with more accurate naming
    Also update documentation as per Tomdoc 1.0.0-rc1

--- primes.rb
+++ primes.rb
@@ -1,22 +1,33 @@
-
-# is_prime?(number)
-#
-# Returns true if the provided number has no divisors besides itself and 1
+# Public: Returns true if the provided number has no divisors besides itself
+# and 1
 #
 # number - A positive integer
 def is_prime? number
-  divs = find_divisors number
-  divs.empty?
+  root_floor = (number ** 0.5).floor
+
+  # Only check if there are divisors up to the square root.  If there are any
+  # divisors greater than the square root, there must be divisors less than the
+  # square root
+  divisors_up_to_square_root = nontrivial_divisors_of(number, {upper_limit: root_floor})
+  divisors_up_to_square_root.empty?
 end
 
-# find_divisors(primary_num)
-#
-# Returns an array of divisors of primary_num, excluding 1 and primary_num
+# Public: Returns an array of divisors of primary_num, excluding 1 and
+# primary_num
 #
 # primary_num - A positive integer
-def find_divisors primary_num
+# options     - A Hash that can optionally alter the list of primes returned
+#               :upper_limit - The Integer upper_limit of the divisors list,
+#                              inclusive.
+def nontrivial_divisors_of primary_num, options={}
+  # Merge in default options
+  DEFAULT_OPTIONS = {upper_limit: primary_num}
 
-  root = primary_num ** 0.5
+  # Because default options in the argument list will be ignored/missed if a
+  # partial option hash is passed in
+  options = DEFAULT_OPTIONS.merge options
+
+  upper_limit = options[:upper_limit]
   c = 2..(root.floor)
 
   divisors = []
