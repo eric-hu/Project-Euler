3e7ea09b24cc72cdddeb6b069a395f9b6d0d2669
Eric Hu <eric@lemurheavy.com>
2013-04-13 15:48:42 -0700

    Assertion: add notes for future assertion message feature

--- tests/assertion.rb
+++ tests/assertion.rb
@@ -8,3 +8,12 @@ end
 def assert &block
   raise AssertionError unless yield
 end
+
+# class Object
+#   # example:
+#   #   foo.should(eq(20))
+#   # "foo should equal 20, but was 10" on failure
+#   def should
+#
+#   end
+# end
